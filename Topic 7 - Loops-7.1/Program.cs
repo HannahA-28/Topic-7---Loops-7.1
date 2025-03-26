5b98bdaac526cc1e8744f0f03cb1aaff158a357c
Hannah Anderson <339729345@GoTVDSB.ca>
2025-03-26 14:39:30 -0400

    Onto task one!

--- Topic 7 - Loops-7.1/Program.cs
+++ Topic 7 - Loops-7.1/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //while (true)
+            //    Console.WriteLine("Are we there yet?");
+
+            //int pin = 12345;
+            //int entry;
+            //Console.WriteLine("WELCOME TO THE BANK OF ALDWORTH.");
+            //Console.WriteLine("ENTER YOUR PIN: ");
+            //int.TryParse(Console.ReadLine(), out entry);
+            //Console.WriteLine();
+            //while (entry != pin)
+            //{
+            //    Console.WriteLine("INCORRECT PIN. TRY AGAIN.");
+            //    Console.WriteLine("ENTER YOUR PIN: ");
+            //    int.TryParse(Console.ReadLine(), out entry);
+            //    Console.WriteLine();
+            //}
+            //Console.WriteLine("PIN ACCEPTED. YOU NOW HAVE ACCESS TO YOUR ACCOUNT.");
+
+            //int age;
+            //while (!int.TryParse(Console.ReadLine(), out age))
+            //    Console.WriteLine("Please enter a valid number.");
+
+            Console.WriteLine("Task one");
+            Random generator = new Random();
+            int guess, secretNumber;
+            secretNumber = generator.Next(1, 11);
+            Console.WriteLine("Guess the random number from 1 to 10.");
+
+
+            //Random generator = new Random();
+            //int guess, secretNumber;
+            //secretNumber = generator.Next(1, 11);
+            //Console.WriteLine("Guess the random number from 1 to 10.");
+            //guess = Convert.ToInt32(Console.ReadLine());
+            //if (guess == secretNumber)
+            //    Console.WriteLine("Congratulations, you guessed it!");
+            //else
+            //    Console.WriteLine("Wrong, the secret number was " + secretNumber);
         }
     }
 }
